2adaac02badc0a246b91adb44ed42390dd8b7bd8
leduong0311 <66908165+leduong0311@users.noreply.github.com>
2020-06-14 19:36:16 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,245 @@
+#include "stm32f4xx.h"
+#include "system_timetick.h"
+
+
+int count_1 = 0, count_2 = 0;
+
+void Delay(int t){
+	int i;
+	for (i=0; i<t; i++);
+}
+
+void delay_us(uint16_t period){
+
+  	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE);
+  	TIM6->PSC = 83;		// clk = SystemCoreClock /2/(PSC+1) = 1MHz
+  	TIM6->ARR = period-1;
+  	TIM6->CNT = 0;
+  	TIM6->EGR = 1;		// update registers;
+
+  	TIM6->SR  = 0;		// clear overflow flag
+  	TIM6->CR1 = 1;		// enable Timer6
+
+  	while (!TIM6->SR);
+    
+  	TIM6->CR1 = 0;		// stop Timer6
+  	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, DISABLE);
+}
+
+void delay_01ms(uint16_t period){
+
+  	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE);
+  	TIM6->PSC = 8399;		// clk = SystemCoreClock /2 /(PSC+1) = 10KHz
+  	TIM6->ARR = period-1;
+  	TIM6->CNT = 0;
+  	TIM6->EGR = 1;		// update registers;
+
+  	TIM6->SR  = 0;		// clear overflow flag
+  	TIM6->CR1 = 1;		// enable Timer6
+
+  	while (!TIM6->SR);
+    
+  	TIM6->CR1 = 0;		// stop Timer6
+  	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, DISABLE);
+}
+
+ 
+
+void init_main(void);
+
+void TIM_Config(void);
+int main(void)
+{
+	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+  TIM_OCInitTypeDef  TIM_OCInitStructure;
+	
+uint16_t CCR1_Val = 333;
+uint16_t CCR2_Val = 249;
+uint16_t CCR3_Val = 166;
+uint16_t CCR4_Val = 83;
+uint16_t PrescalerValue = 0;
+
+  /*!< At this stage the microcontroller clock setting is already configured, 
+       this is done through SystemInit() function which is called from startup
+       files (startup_stm32f40_41xxx.s/startup_stm32f427_437xx.s/startup_stm32f429_439xx.s)
+       before to branch to application main. 
+       To reconfigure the default setting of SystemInit() function, refer to
+       system_stm32f4xx.c file
+     */
+
+  /* TIM Configuration */
+  TIM_Config();
+  
+  /* -----------------------------------------------------------------------
+    TIM3 Configuration: generate 4 PWM signals with 4 different duty cycles.
+    
+    In this example TIM3 input clock (TIM3CLK) is set to 2 * APB1 clock (PCLK1), 
+    since APB1 prescaler is different from 1.   
+      TIM3CLK = 2 * PCLK1  
+      PCLK1 = HCLK / 4 
+      => TIM3CLK = HCLK / 2 = SystemCoreClock /2
+          
+    To get TIM3 counter clock at 21 MHz, the prescaler is computed as follows:
+       Prescaler = (TIM3CLK / TIM3 counter clock) - 1
+       Prescaler = ((SystemCoreClock /2) /21 MHz) - 1
+                                              
+    To get TIM3 output clock at 30 KHz, the period (ARR)) is computed as follows:
+       ARR = (TIM3 counter clock / TIM3 output clock) - 1
+           = 665
+                  
+    TIM3 Channel1 duty cycle = (TIM3_CCR1/ TIM3_ARR)* 100 = 50%
+    TIM3 Channel2 duty cycle = (TIM3_CCR2/ TIM3_ARR)* 100 = 37.5%
+    TIM3 Channel3 duty cycle = (TIM3_CCR3/ TIM3_ARR)* 100 = 25%
+    TIM3 Channel4 duty cycle = (TIM3_CCR4/ TIM3_ARR)* 100 = 12.5%
+
+    Note: 
+     SystemCoreClock variable holds HCLK frequency and is defined in system_stm32f4xx.c file.
+     Each time the core clock (HCLK) changes, user had to call SystemCoreClockUpdate()
+     function to update SystemCoreClock variable value. Otherwise, any configuration
+     based on this variable will be incorrect.    
+  ----------------------------------------------------------------------- */   
+  
+  /* Compute the prescaler value */
+  PrescalerValue = (uint16_t) ((SystemCoreClock /2) / 21000000) - 1;
+  /* Time base configuration */
+  TIM_TimeBaseStructure.TIM_Period = 665;
+  TIM_TimeBaseStructure.TIM_Prescaler = PrescalerValue;
+  TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+
+  TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+
+  /* PWM1 Mode configuration: Channel1 */
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR1_Val;
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+  TIM_OC1Init(TIM3, &TIM_OCInitStructure);
+
+  TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
+  /* PWM1 Mode configuration: Channel2 */
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR2_Val;
+
+  TIM_OC2Init(TIM3, &TIM_OCInitStructure);
+
+  TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
+
+  /* PWM1 Mode configuration: Channel3 */
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR3_Val;
+
+  TIM_OC3Init(TIM3, &TIM_OCInitStructure);
+
+  TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
+
+  /* PWM1 Mode configuration: Channel4 */
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR4_Val;
+
+  TIM_OC4Init(TIM3, &TIM_OCInitStructure);
+
+  TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
+
+  TIM_ARRPreloadConfig(TIM3, ENABLE);
+
+  /* TIM3 enable counter */
+  TIM_Cmd(TIM3, ENABLE);
+
+  while (1)
+  {
+		TIM_OCInitStructure.TIM_Pulse += 50;
+		if(TIM_OCInitStructure.TIM_Pulse > 665) TIM_OCInitStructure.TIM_Pulse = 0;
+		TIM_OC2Init(TIM3, &TIM_OCInitStructure);
+		TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
+		if(GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 1){
+			while(GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 1);
+		GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
+		delay_01ms(10000);
+		GPIO_ToggleBits(GPIOD, GPIO_Pin_13);
+		delay_01ms(10000);
+		GPIO_ToggleBits(GPIOD, GPIO_Pin_14);
+		delay_01ms(10000);
+		GPIO_ToggleBits(GPIOD, GPIO_Pin_15);
+		delay_01ms(10000);
+			}
+	}
+}
+
+/**
+  * @brief  Configure the TIM3 Output Channels.
+  * @param  None
+  * @retval None
+  */
+void TIM_Config(void)
+{
+	
+	
+	/////////////
+  GPIO_InitTypeDef GPIO_InitStructure;
+  /* TIM3 clock enable */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+  /* GPIOC clock enable */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+  
+  /* GPIOC Configuration: TIM3 CH1 (PC6), TIM3 CH2 (PC7), TIM3 CH3 (PC8) and TIM3 CH4 (PC9) */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
+  GPIO_Init(GPIOC, &GPIO_InitStructure); 
+
+  /* Connect TIM3 pins to AF2 */  
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource6, GPIO_AF_TIM3);
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource7, GPIO_AF_TIM3); 
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource8, GPIO_AF_TIM3);
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource9, GPIO_AF_TIM3); 
+	/* cau hinh chan pd7 nhay*/
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	////////////////
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  while (1)
+  {}
+}
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
